d58fdbf13f6c2a19541f18d41aa57fc15aa1233f
Anders Nygren <litemerafrukt@gmail.com>
2017-11-02 11:42:19 +0100

    Travis and refactor state to union type

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Me-sida för ramverk2
+[![Build Status](https://travis-ci.org/litemerafrukt/ramverk2.svg?branch=master)](https://travis-ci.org/litemerafrukt/ramverk2)
 
-## Installera och testa
+## Installera
 1. Klona repot
 2. cd in
 3. `npm install`
@@ -8,5 +9,8 @@
 5. Seriöst, det tar tid, gör lite te om kaffe inte är grejen.
 6. `npm start`
 7. Servern lyssnar på port angiven i `DBWEBB_PORT`, `PORT`, `LOCAL_DEV_PORT` eller `3000` i den ordningen.
+
+## Köra tester
+`npm test` kör tester och linters för server och client.
 
 Licens MIT
--- client/src/pages/Report.js
+++ client/src/pages/Report.js
@@ -1,28 +1,37 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import axios from "axios";
-import { lensProp, set, view } from "ramda";
+import { lensProp, set } from "ramda";
 import { withRouter } from "react-router-dom";
+import Type from "union-type";
 
-const textLens = lensProp("text");
+/**
+ * State type
+ */
+const ReportState = Type({
+    Nothing: [],
+    Fetching: [],
+    KmomReport: [String],
+    Fail: [String]
+});
+
+const contentLens = lensProp("content");
+
+const { Nothing, Fetching, KmomReport, Fail } = ReportState;
 
 class Report extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            kmom: props.match.params.kmom,
-            text: "wait for it..."
-        };
-    }
-
-    setStateText(text) {
-        this.setState(set(textLens, text));
+        this.state = { content: Nothing };
     }
 
     fetchReport({ kmom }) {
+        this.setState(set(contentLens, Fetching));
         axios(`/api/reports/${kmom}`)
-            .then(({ data }) => this.setStateText(data))
-            .catch(() => this.setStateText("Fel vid rapporthämtning. Är backend igång?"));
+            .then(({ data }) => this.setState(set(contentLens, KmomReport(data))))
+            .catch(() =>
+                this.setState(set(contentLens, Fail("Fel vid rapporthämtning. Är backend igång?")))
+            );
     }
 
     componentDidMount() {
@@ -34,8 +43,49 @@ class Report extends React.Component {
     }
 
     render() {
-        return <ReactMarkdown source={view(textLens, this.state)} />;
+        return ReportState.case(
+            {
+                Nothing: () => <div />,
+                Fetching: () => <h2>vänta lite bara...</h2>,
+                Fail: message => <pre>{message}</pre>,
+                KmomReport: text => <ReactMarkdown source={text} />
+            },
+            this.state.content
+        );
     }
 }
+
+// const textLens = lensProp("text");
+
+// class Report extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             text: "wait for it..."
+//         };
+//     }
+
+//     setStateText(text) {
+//         this.setState(set(textLens, text));
+//     }
+
+//     fetchReport({ kmom }) {
+//         axios(`/api/reports/${kmom}`)
+//             .then(({ data }) => this.setStateText(data))
+//             .catch(() => this.setStateText("Fel vid rapporthämtning. Är backend igång?"));
+//     }
+
+//     componentDidMount() {
+//         this.fetchReport(this.props.match.params);
+//     }
+
+//     componentWillReceiveProps(nextProps) {
+//         this.fetchReport(nextProps.match.params);
+//     }
+
+//     render() {
+//         return <ReactMarkdown source={view(textLens, this.state)} />;
+//     }
+// }
 
 export default withRouter(Report);
