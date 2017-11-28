8065eda0eaabc48908d08f8ff2368371f41b0c00
Anders Nygren <litemerafrukt@gmail.com>
2017-11-28 10:08:02 +0100

    try to make tests travis friendly

--- app.test.js
+++ app.test.js
@@ -7,38 +7,39 @@ const request = require("supertest");
 
 const app = require("./app");
 
-test("It should response to GET method on home with status 200", () => {
+test("It should response with status 200 on home (/)", done => {
     return request(app)
         .get("/")
-        .expect(200);
+        .expect(200, done);
 });
 
-test("It should response to GET method on /whatever with status 404", () => {
+test("It should with status 404 on /whatever", done => {
     return request(app)
         .get("/whatever")
-        .expect(404);
+        .expect(404, done);
 });
 
-test("/api/test should respond with status 200", () => {
+test("/api/test should respond with status 200", done => {
     return request(app)
         .get("/api/test")
-        .expect(200);
+        .expect(200, done);
 });
 
-test("/api/test should respond with body containing 'test' key", () => {
+test("/api/test should respond with body containing 'test' key", done => {
     return request(app)
         .get("/api/test")
-        .then(res => expect(res.body).toHaveProperty("test"));
+        .then(res => expect(res.body).toHaveProperty("test"))
+        .then(done);
 });
 
-test("/api/reports/kmom01 should respond with 200", () => {
+test("/api/reports/kmom01 should respond with 200", done => {
     return request(app)
         .get("/api/reports/kmom01")
-        .expect(200);
+        .expect(200, done);
 });
 
-test("/api/reports/whatever should respond with 404", () => {
+test("/api/reports/whatever should respond with 404", done => {
     return request(app)
         .get("/api/reports/whatever")
-        .expect(404);
+        .expect(404, done);
 });
